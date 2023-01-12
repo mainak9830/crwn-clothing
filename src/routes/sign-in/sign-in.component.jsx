dcb771ce03a5e93fd484566991e49d1df3bd0b88
mainak9830 <madak9830@gmail.com>
2023-01-12 00:07:37 -0500

    create signup page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
-
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { 
+    auth,
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect 
+} from '../../utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 const SignIn = () => {
+    
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = createUserDocumentFromAuth(user);
+        const userDocRef = await createUserDocumentFromAuth(user);
         console.log(userDocRef)
     };
-
+    
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <SignUpForm/>
         </div>
     );
 };
